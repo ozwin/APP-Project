cb2dd6c30bc73d4fcd62591f6ce14cefb2f311ba
Ozwin <ozwinneellobo12@gmail.com>
2023-03-10 23:51:47 -0500

    more tests!

--- src/test/java/com/app/project/service/PropertyServicesTest.java
+++ src/test/java/com/app/project/service/PropertyServicesTest.java
@@ -1,9 +1,6 @@
 package com.app.project.service;
 
-import com.app.project.entity.Address;
-import com.app.project.entity.Apartment;
-import com.app.project.entity.Condo;
-import com.app.project.entity.PrivateHouse;
+import com.app.project.entity.*;
 import com.app.project.interfaces.IProperty;
 import com.app.project.repository.PropertiesRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -18,8 +15,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class PropertyServicesTest {
     private static PropertiesRepository repository;
@@ -46,9 +42,9 @@ class PropertyServicesTest {
     void addTestSuccess() {
         Apartment apartment=new Apartment(2,2,200,new Address("St Catherine","Montreal","H3H1K4"));
         UUID propertyID=apartment.getPropertyId();
-        propertyServices.add(apartment);
         when(repository.insert(apartment)).thenReturn(true);
         when(repository.findByKey(propertyID)).thenReturn(apartment);
+        propertyServices.add(apartment);
         IProperty property=propertyServices.getByKey(propertyID);
         assertEquals(apartment,property);
     }
@@ -56,8 +52,10 @@ class PropertyServicesTest {
     @Test
     void getByKeyTestSuccess() {
         Apartment apartment=new Apartment(2,2,200,new Address("St Catherine","Montreal","H3H1K4"));
-        propertyServices.add(apartment);
         UUID propertyID=apartment.getPropertyId();
+        when(repository.insert(apartment)).thenReturn(true);
+        when(repository.findByKey(propertyID)).thenReturn(apartment);
+        propertyServices.add(apartment);
         IProperty property=propertyServices.getByKey(propertyID);
         assertEquals(apartment,property);
     }
@@ -68,12 +66,14 @@ class PropertyServicesTest {
         Condo condo=new Condo("23","2",new Address("St Catherine","Montreal","H3H1K4"));
         PrivateHouse house=new PrivateHouse("54",new Address("St Catherine","Montreal","H3H1K4"));
         ArrayList<IProperty> properties=propertyServices.getAll();
+        when(repository.insert(any())).thenReturn(true);
         propertyServices.add(apartment);
         propertyServices.add(condo);
         propertyServices.add(house);
         properties.add(apartment);
         properties.add(condo);
         properties.add(house);
+        when(repository.getAll()).thenReturn(properties);
         ArrayList<IProperty> rentalProperties=propertyServices.getAll();
         assertEquals(properties,rentalProperties);
     }
@@ -82,11 +82,28 @@ class PropertyServicesTest {
     void findVacantTestSuccess() {
         Apartment apartment=new Apartment(2,2,200,new Address("St Catherine","Montreal","H3H1K4"));
         Condo condo=new Condo("23","2",new Address("St Catherine","Montreal","H3H1K4"));
-
+        doAnswer((i)-> {return null;}).when(repository).upsert(apartment);
+        ArrayList<IProperty> vacantProperties=new ArrayList<IProperty>();
+        vacantProperties.add(condo);
+        when(repository.findByKey(apartment.getPropertyId())).thenReturn(apartment);
+        when(repository.findVacant()).thenReturn(vacantProperties);
+        propertyServices.assignATenant(new Tenant("Ozwin","Lobo",new Contact("xxx@gmail.com","90807333"),apartment.getPropertyId()));
+        ArrayList<IProperty> properties=propertyServices.findVacant();
+        assertEquals(properties,vacantProperties);
     }
 
     @Test
     void findRentedSuccess() {
+        Apartment apartment=new Apartment(2,2,200,new Address("St Catherine","Montreal","H3H1K4"));
+        Condo condo=new Condo("23","2",new Address("St Catherine","Montreal","H3H1K4"));
+        doAnswer((i)-> {return null;}).when(repository).upsert(apartment);
+        ArrayList<IProperty> rentedProperties=new ArrayList<IProperty>();
+        rentedProperties.add(apartment);
+        when(repository.findByKey(apartment.getPropertyId())).thenReturn(apartment);
+        when(repository.findRented()).thenReturn(rentedProperties);
+        propertyServices.assignATenant(new Tenant("Ozwin","Lobo",new Contact("xxx@gmail.com","90807333"),apartment.getPropertyId()));
+        ArrayList<IProperty> properties=propertyServices.findRented();
+        assertEquals(properties,rentedProperties);
     }
 
     @Test
